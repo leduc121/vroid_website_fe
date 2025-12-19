f357472804c2657ca2ac1bf3564d6d4b2b88f5f2
duc <quangducle.127@gmail.com>
2025-12-20 00:55:10 +0700

    Fix: Set base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` : '/',
+  base: '/vroid_website_fe/',
 })
